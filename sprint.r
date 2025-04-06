b19de36cbab59549da88337cbd038fcdf61b99fe
Riddha T <riddha.t@gmail.com>
2025-04-06 14:13:38 -0700

    split data

--- sprint.r
+++ sprint.r
@@ -4,7 +4,6 @@ library(xgboost) #XGBoost library
 library(mltools)
 library(ggplot2)
 
-
 ai_jobs <- read.csv("ai_job_market_insights.csv")
 
 head(ai_jobs)
@@ -12,6 +11,14 @@ head(ai_jobs)
 med_sal <- median(ai_jobs$Salary_USD)
 
 ai_jobs$Salary_USD <- ifelse(ai_jobs$Salary_USD >= med_sal, 1, 0)
+
+split <- createDataPartition(ai_jobs$Salary_USD, p = 0.75, list = FALSE)
+train <- ai_jobs[split, ]
+test <- ai_jobs[-split, ]
+
+train_x <- data.matrix(select(train, -Salary_USD))
+train_y <- data.matrix(select(train, Salary_USD))
+
 
 # Get column names
 col_names <- colnames(ai_jobs)
