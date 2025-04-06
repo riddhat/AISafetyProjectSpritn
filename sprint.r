664d88d36a807576a19b67ad4aef402ff95c3d75
Nancy <yma60@student.ubc.ca>
2025-04-06 14:18:53 -0700

    split

--- sprint.r
+++ sprint.r
@@ -15,7 +15,15 @@ print(col_names)
 
 summary(ai_jobs)
 
+
 med_sal <- median(ai_jobs$Salary_USD, na.rm=TRUE)
 
 ai_jobs$Salary_USD <- ifelse(ai_jobs$Salary_USD >= med_sal, 1, 0)
 
+#We set the seed so the randomization can be reproducible
+set.seed(123)
+
+#Split the data to testing and training sets
+split_ai_job <- initial_split(data= ai_jobs, prop = 0.75, strata = Salary_USD)
+ai_job_training <- training(split_ai_job)
+ai_job_testing <- testing(split_ai_job)
