6441970a5cb4661307f7842a79886e16097bbe7c
Riddha T <riddha.t@gmail.com>
2025-04-06 14:58:48 -0700

    as.matrix

--- sprint.r
+++ sprint.r
@@ -26,6 +26,7 @@ med_sal <- median(ai_jobs$Salary_USD, na.rm = TRUE)
 
 ai_jobs$Salary_USD <- ifelse(ai_jobs$Salary_USD >= med_sal, ">= median($91k)", "< median ($91k)")
 
+ai_jobs <- ai_jobs %>% mutate_all(as.factor)
 
 #We set the seed so the randomization can be reproducible
 set.seed(123)
@@ -35,14 +36,15 @@ split <- createDataPartition(ai_jobs$Salary_USD, p = 0.75, list = FALSE)
 train <- ai_jobs[split, ]
 test <- ai_jobs[-split, ]
 
-train_x <- data.matrix(select(train, -Salary_USD)) #train on every other variable except Salary
-train_y <- data.matrix(select(train, Salary_USD))  #response variable matrix
+train_x <- as.matrix(select(train, -Salary_USD)) #train on every other variable except Salary
+train_y <- as.matrix(select(train, Salary_USD))  #response variable matrix
 
-test_x <- data.matrix(select(test, -Salary_USD))
-test_y <- data.matrix(select(test, Salary_USD))
+test_x <- as.matrix(select(test, -Salary_USD))
+test_y <- as.matrix(select(test, Salary_USD))
 
 xgb_train <- xgb.DMatrix(data = train_x, label = train_y)
 xgb_test <- xgb.DMatrix(data = test_x, label = test_y)
 
 head(ai_jobs)
 head(train_x)
+
