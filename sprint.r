f2d8441a395235683fbe3b9ca952a5977aa9634d
Riddha T <riddha.t@gmail.com>
2025-04-06 14:34:51 -0700

    my changes

--- sprint.r
+++ sprint.r
@@ -8,9 +8,9 @@ ai_jobs <- read.csv("ai_job_market_insights.csv")
 
 head(ai_jobs)
 
-med_sal <- median(ai_jobs$Salary_USD)
+med_sal <- median(ai_jobs$Salary_USD, na.rm = TRUE)
 
-ai_jobs$Salary_USD <- ifelse(ai_jobs$Salary_USD >= med_sal, ">=median salary($91k)", "<median salary($91k)")
+ai_jobs$Salary_USD <- ifelse(ai_jobs$Salary_USD >= med_sal, 1, 0)
 
 
 #We set the seed so the randomization can be reproducible
@@ -21,8 +21,16 @@ split <- createDataPartition(ai_jobs$Salary_USD, p = 0.75, list = FALSE)
 train <- ai_jobs[split, ]
 test <- ai_jobs[-split, ]
 
-train_x <- data.matrix(select(train, -Salary_USD)) #train on every other variable except Salary
-train_y <- data.matrix(select(train, Salary_USD))  
+train_x <- data.matrix(select(train, -Salary_USD))
+train_y <- data.matrix(select(train, Salary_USD))
+
+test_x <- data.matrix(select(test, -Salary_USD))
+test_y <- data.matrix(select(test, Salary_USD))
+
+xgb_train <- xgb.DMatrix(data = train_x, label = train_y)
+xgb_test <- xgb.DMatrix(data = test_x, label = test_y)
+
+
 
 
 # Get column names
@@ -30,3 +38,4 @@ col_names <- colnames(ai_jobs)
 print(col_names)
 
 summary(ai_jobs)
+
